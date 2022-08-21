af554e07962e516fccb557c834746d76527c4a37
Kaysie <kaysielandersonwork@gmail.com>
2022-08-21 15:43:18 -0500

    update

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -5,6 +5,12 @@ const pizzaController = {
 
     getAllPizza(req, res) {
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1})
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.og(err);
@@ -16,6 +22,11 @@ const pizzaController = {
 
     getPizzaById({params}, res) {
         Pizza.findOne({ _id: params.id})
+        .populate({
+            path: 'comments',
+            select: '-__v'
+          })
+          .select('-__v')
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({message: 'no pizza found with this id'});
